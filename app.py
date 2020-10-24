876b86c4fd3b8e0397a69e83d2c2846af3027c01
rojabalakrishnan <rojabalakrishnan69@gmail.com>
2020-10-24 02:27:17 -0700

    Included feature to add new job from the UserInterface

--- app.py
+++ app.py
@@ -68,6 +68,24 @@ def edit(id):
     else:
         return render_template('edit.html', job_post=job_post)
 
+@app.route('/posts/new', methods=['GET', 'POST'])
 
+def new_post():
+    if request.method == 'POST':
+        job_title = request.form['title']
+        job_description = request.form['description']
+        pay_per_hour = request.form['pay_per_hour']
+        expiry_date = request.form['expiry_date']
+
+        expiry_date_old = expiry_date.replace('T', ' ')
+        expiry_date_new = datetime.strptime(expiry_date_old, '%Y-%m-%d %H:%M')
+
+        new_job_post = JobPost(title=job_title, description=job_description, pay_per_hour=pay_per_hour,
+                               expiry_date=expiry_date_new)
+        db.session.add(new_job_post)
+        db.session.commit()
+        return redirect('/posts')
+    else:
+        return render_template('new_post.html')
 if __name__ == "__main__":
     app.run(debug=True)
