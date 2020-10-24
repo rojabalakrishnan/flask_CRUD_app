a6f0a64a1809de5b369d4347b892394d8069ecb4
rojabalakrishnan <rojabalakrishnan69@gmail.com>
2020-10-24 00:17:04 -0700

    Added Edit and Delete functionality to the flask application.

--- app.py
+++ app.py
@@ -42,6 +42,31 @@ def posts():
         return render_template('posts.html', job_posts=all_job_posts)
 
 
+@app.route('/posts/delete/<int:id>')
+def delete(id):
+    job_post = JobPost.query.get_or_404(id)
+    db.session.delete(job_post)
+    db.session.commit()
+    return redirect('/posts')
+
+
+@app.route('/posts/edit/<int:id>', methods=['GET', 'POST'])
+def edit(id):
+    job_post = JobPost.query.get_or_404(id)
+
+    if request.method == 'POST':
+        job_post.title = request.form['title']
+        job_post.description = request.form['description']
+        job_post.pay_per_hour = request.form['pay_per_hour']
+        expiry_date = request.form['expiry_date']
+        expiry_date_old = expiry_date.replace('T', ' ')
+        expiry_date_new = datetime.strptime(expiry_date_old, '%Y-%m-%d %H:%M:%S')
+
+        job_post.expiry_date = expiry_date_new
+        db.session.commit()
+        return redirect('/posts')
+    else:
+        return render_template('edit.html', job_post=job_post)
 
 
 if __name__ == "__main__":
